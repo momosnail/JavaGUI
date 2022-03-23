986b89d7494fb4d96242aed07a350549a6a655f6
momosnail <550218658@qq.com>
2022-03-23 22:32:22 +0800

    FlowLayoutDemo

---
+++ src/main/java/awt/container/FlowLayoutDemo.java
@@ -0,0 +1,22 @@
+package awt.container;
+
+import java.awt.*;
+
+public class FlowLayoutDemo {
+    public static void main(String[] args) {
+        Frame frame=new Frame("这里测试FlowLayout");
+
+        // 1.通过setLayout方法设置容器的布局管理器 默认为：BorderLayout布局管理器
+        frame.setLayout(new FlowLayout(FlowLayout.CENTER,20,20));
+
+        // 2.添加多个按钮到frame中
+        for (int i = 0; i <= 100; i++) {
+            frame.add(new Button("按钮"+i));
+        }
+
+        // 3.设置最佳大小，pack方法
+        frame.pack();
+
+        frame.setVisible(true);
+    }
+}
--- src/main/java/awt/container/WindowDemo.java
+++ src/main/java/awt/container/WindowDemo.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 public class WindowDemo {
     public static void main(String[] args) {
-        // 1.创建创后对象
+        // 1.创建创后对象 Frame是Window的子类
         Frame frame = new Frame("这是第一个窗口容器");
 
         // 2.设置窗口位置和大小
